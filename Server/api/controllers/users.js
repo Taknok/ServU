fe376fbb39e409b5e4f1c3deac4a793ad47c93f0
alexandregermain <agermain@enseirb-matmeca.fr>
2017-04-08 12:40:21 +0200

    Integration session et page inscription / profil

--- Server/api/controllers/users.js
+++ Server/api/controllers/users.js
@@ -1,6 +1,6 @@
 'use strict';
 var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+    , assert = require('assert');
 
 // Connection URL
 var url = 'mongodb://127.0.0.1:27017/ServU';
@@ -9,284 +9,283 @@ var util = require('util');
 
 
 module.exports = {
-  postUsers,
-  getUsersUsername,
-  putUsersUsername,
-  deleteUsersUsername,
-  getUsersDevice,
-  postUsersDevice,
-  getUsersDeviceUuid,
-  deleteUsersDeviceUuid,
-  getUsersDeviceUuidProbes,
-  getUsersDeviceUuidProbesName,
-  postUsersDeviceUuidActions
+    postUsers,
+    getUsersUsername,
+    putUsersUsername,
+    deleteUsersUsername,
+    getUsersDevice,
+    postUsersDevice,
+    getUsersDeviceUuid,
+    deleteUsersDeviceUuid,
+    getUsersDeviceUuidProbes,
+    getUsersDeviceUuidProbesName,
+    postUsersDeviceUuidActions
 }
 //utilisé les $set
 
+// POST users
 function postUsers(req, res, next) {
-  MongoClient.connect(url,  function(err, db1) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-    db1.collection("users").findOne({"username": req.body.username},function(error, exist) {
-      if(exist == null && error == null){
-        db1.collection("users").insert(req.body,function(err, probe) {
-             if (!err){
-               res.render('gestion', {
-                   username: req.body.username,
-                   lastname: req.body.lastname,
-                   firstname: req.body.firstname});
-             }
-             else{
-                 res.status(204).send();
-             }
-           }
-        );
-      }
-      else{
-        res.status(409).send();
-      }
+    MongoClient.connect(url,  function(err, db1) {
+        assert.equal(null, err);
+        db1.collection("users").findOne({"username": req.body.username},function(error, exist) {
+            if(exist == null && error == null){
+                db1.collection("users").insert(req.body,function(err, probe) {
+                        if (!err){
+                            req.session.username = req.body.username;
+                            req.session.lastname = req.body.lastname;
+                            req.session.firstname = req.body.firstname;
+                            res.redirect('/api/users/' + req.body.username);
+                        }
+                        else{
+                            res.status(204).send();
+                        }
+                    }
+                );
+            }
+            else{
+                res.status(409).send();
+                res.redirect('/');
+            }
+        });
     });
-  });
 }
 
+// GET /users/{username}
 function getUsersUsername(req, res, next) {
-  MongoClient.connect(url,  function(err, db1) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-    console.log(req.body);
-    db1.collection("users").findOne({"username": req.swagger.params.username.value},function(error, use) {
-      if(use != null && error == null){
-        var user = {"username": use.username, "firstname":use.firstname, "lastname": use.lastname, "email": use.email, "devices": use.devices};
-        res.render('gestion', {
-            username: req.body.username,
-            lastname: req.body.lastname,
-            firstname: req.body.firstname});
-      }
-      else{
-        res.status(409).send();
-      }
+    MongoClient.connect(url,  function(err, db1) {
+        assert.equal(null, err);
+        db1.collection("users").findOne({"username": req.session.username}, function (error, exist) {
+            if (exist != null && error == null && typeof(req.session.username) != 'undefined'){
+                res.render('gestion', {
+                    username: req.session.username,
+                    lastname: req.session.lastname,
+                    firstname: req.session.firstname
+                });
+            }
+            else{
+                res.status(409).send();
+            }
+        });
     });
-
-  });
 }
 
 // /PUT  /phone/{uuid}/probes
-    function putUsersUsername(req, res, next) {
-      MongoClient.connect(url,  function(err, db1) {
+function putUsersUsername(req, res, next) {
+    MongoClient.connect(url,  function(err, db1) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
         db1.collection("users").findOne({"username": req.body.username},function(error2, use2){
-          if(use2 == null){
-            db1.collection("users").findOne({"username": req.swagger.params.username.value},function(error, use) {
-              if(use != null && error == null){
-                if (req.body.username) use.username = req.body.username;
-                if (req.body.lastname) use.lastname = req.body.lastname;
-                if (req.body.firstname) use.firstname = req.body.firstname;
-                if (req.body.email) use.email = req.body.email;
-                if (req.body.password) use.password = req.body.password;
-                db1.collection("users").update({"username" : req.swagger.params.username.value}, use,  function(err2, modif){
-                  if (!err2) res.json({success: 1, description: "Users modifié"});
-                  else{
-                       res.status(400).send();
-                  }
+            if(use2 == null){
+                db1.collection("users").findOne({"username": req.swagger.params.username.value},function(error, use) {
+                    if(use != null && error == null){
+                        if (req.body.username) use.username = req.body.username;
+                        if (req.body.lastname) use.lastname = req.body.lastname;
+                        if (req.body.firstname) use.firstname = req.body.firstname;
+                        if (req.body.email) use.email = req.body.email;
+                        if (req.body.password) use.password = req.body.password;
+                        db1.collection("users").update({"username" : req.swagger.params.username.value}, use,  function(err2, modif){
+                            if (!err2) res.json({success: 1, description: "Users modifié"});
+                            else{
+                                res.status(400).send();
+                            }
+                        });
+                    }
+                    else{
+                        res.status(404).send();
+                    }
                 });
-              }
-              else{
-                res.status(404).send();
-              }
-            });
-          }
-          else{
-            res.status(409).send();
-          }
+            }
+            else{
+                res.status(409).send();
+            }
         });
-      });
-    }
+    });
+}
 
 
-    function deleteUsersUsername(req, res, next) {
-      MongoClient.connect(url,  function(err, db1) {
+function deleteUsersUsername(req, res, next) {
+    MongoClient.connect(url,  function(err, db1) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
         db1.collection("users").findOne({"username": req.swagger.params.username.value},function(error, exist) {
-          if(exist != null && error == null){
-            db1.collection("users").remove( { "username": req.swagger.params.username.value },function(err, val) {
-                 if (!err) res.json({success: 1, description: "Users ajouté"});
-                 else{
-                     res.status(404).send();
-                 }
-               }
-            );
-          }
-          else{
-            res.status(404).send();
-          }
+            if(exist != null && error == null){
+                db1.collection("users").remove( { "username": req.swagger.params.username.value },function(err, val) {
+                        if (!err) res.json({success: 1, description: "Users ajouté"});
+                        else{
+                            res.status(404).send();
+                        }
+                    }
+                );
+            }
+            else{
+                res.status(404).send();
+            }
         });
 
-      });
-    }
+    });
+}
 
 
-    function getUsersDevice(req, res, next) {
-      MongoClient.connect(url,  function(err, db1) {
+function getUsersDevice(req, res, next) {
+    MongoClient.connect(url,  function(err, db1) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
         db1.collection("users").findOne({"username": req.swagger.params.username.value},function(error, use) {
-          if(use != null && error == null){
-           res.json(use.devices);
-          }
-          else{
-            res.status(409).send();
-          }
+            if(use != null && error == null){
+                res.json(use.devices);
+            }
+            else{
+                res.status(409).send();
+            }
         });
 
-      });
-    }
+    });
+}
 
 
-    function postUsersDevice(req, res, next) {
-      MongoClient.connect(url,  function(err, db1) {
+function postUsersDevice(req, res, next) {
+    MongoClient.connect(url,  function(err, db1) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
         db1.collection("users").findOne({"username": req.swagger.params.username.value},function(error, user) {
-          if(user != null && error == null){
-            var tmp = "devices." + String([req.body.uuid]);
-            const device = { "$set" : {[tmp] : req.body}};
-              db1.collection("users").update({"username" : req.swagger.params.username.value},  device,  function(err2, modif){
-                if (!err2) res.json({success: 1, description: "Users modifié"});
-                else{
-                  console.log("1");
-                     res.status(400).send();
-                }
-              });
-           }
-          else{
-            console.log("2");
-            res.status(404).send();
-          }
+            if(user != null && error == null){
+                var tmp = "devices." + String([req.body.uuid]);
+                const device = { "$set" : {[tmp] : req.body}};
+                db1.collection("users").update({"username" : req.swagger.params.username.value},  device,  function(err2, modif){
+                    if (!err2) res.json({success: 1, description: "Users modifié"});
+                    else{
+                        console.log("1");
+                        res.status(400).send();
+                    }
+                });
+            }
+            else{
+                console.log("2");
+                res.status(404).send();
+            }
         });
-      });
-    }
+    });
+}
 
-    function getUsersDeviceUuid(req, res, next) {
-      MongoClient.connect(url,  function(err, db1) {
+function getUsersDeviceUuid(req, res, next) {
+    MongoClient.connect(url,  function(err, db1) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
         db1.collection("users").findOne({"username": req.swagger.params.username.value},function(error, use) {
-          if(use != null && use.devices[req.swagger.params.uuid.value] != null && error == null){
-            res.json(use.devices[req.swagger.params.uuid.value]);
-          }
-          else{
-            res.status(404).send();
-          }
+            if(use != null && use.devices[req.swagger.params.uuid.value] != null && error == null){
+                res.json(use.devices[req.swagger.params.uuid.value]);
+            }
+            else{
+                res.status(404).send();
+            }
         });
-      });
-    }
+    });
+}
 
-    function deleteUsersDeviceUuid(req, res, next) {
-      MongoClient.connect(url,  function(err, db1) {
+function deleteUsersDeviceUuid(req, res, next) {
+    MongoClient.connect(url,  function(err, db1) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
 
         db1.collection("users").findOne({"username": req.swagger.params.username.value},function(error,use) {
-          if(use != null && use.devices[req.swagger.params.uuid.value] != null && error == null){
+            if(use != null && use.devices[req.swagger.params.uuid.value] != null && error == null){
                 delete(use.devices[req.swagger.params.uuid.value]);
                 const device = { "$set" : {"devices" : use.devices}};
-                  db1.collection("users").update({"username" : req.swagger.params.username.value},  device,  function(err2, modif){
+                db1.collection("users").update({"username" : req.swagger.params.username.value},  device,  function(err2, modif){
                     if (!err2) res.json({success: 1, description: "Users modifié"});
                     else{
-                      console.log("1");
-                         res.status(400).send();
+                        console.log("1");
+                        res.status(400).send();
                     }
-                  });
-          }
-          else res.status(404).send();
+                });
+            }
+            else res.status(404).send();
         });
 
-      });
-    }
+    });
+}
 
 
 // GET /users/{username}/devices/{uuid}/probes
 
 
-    function getUsersDeviceUuidProbes(req, res, next) {
-      MongoClient.connect(url,  function(err, db1) {
+function getUsersDeviceUuidProbes(req, res, next) {
+    MongoClient.connect(url,  function(err, db1) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
         const test = "devices."+String([req.swagger.params.uuid.value]+".uuid")
         db1.collection("users").findOne({"username": req.swagger.params.username.value, [test] : req.swagger.params.uuid.value},function(error, use) {
-          if(use != null && error == null){
-            db1.collection("phone").findOne({"uuid": req.swagger.params.uuid.value},function(err, phone) {
-              if(phone != null && err == null){
-                console.log(phone.probes);
-                res.json(phone.probes)
-              }
-              else{
+            if(use != null && error == null){
+                db1.collection("phone").findOne({"uuid": req.swagger.params.uuid.value},function(err, phone) {
+                    if(phone != null && err == null){
+                        console.log(phone.probes);
+                        res.json(phone.probes)
+                    }
+                    else{
+                        res.status(404).send();
+                    }
+                });
+            }
+            else{
                 res.status(404).send();
-              }
-            });
-          }
-          else{
-            res.status(404).send();
-          }
+            }
         });
-      });
-    }
+    });
+}
 
-    // GET /users/{username}/devices/{uuid}/probes/{name}
-    function getUsersDeviceUuidProbesName(req, res, next) {
-      MongoClient.connect(url,  function(err, db1) {
+// GET /users/{username}/devices/{uuid}/probes/{name}
+function getUsersDeviceUuidProbesName(req, res, next) {
+    MongoClient.connect(url,  function(err, db1) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
         const test = "devices."+String([req.swagger.params.uuid.value]+".uuid")
         db1.collection("users").findOne({"username": req.swagger.params.username.value, [test] : req.swagger.params.uuid.value},function(error, use) {
-          if(use != null && error == null){
-            db1.collection("phone").findOne({"uuid": req.swagger.params.uuid.value, "probes.name":req.swagger.params.name.value},function(err, phone) {
-              if(phone != null && err == null){
-                var i = 0;
-                while(i < phone.probes.length && phone.probes[i].name != req.swagger.params.name.value){
-                  i++;
-                }
-                res.json(phone.probes[i])
-              }
-              else{
+            if(use != null && error == null){
+                db1.collection("phone").findOne({"uuid": req.swagger.params.uuid.value, "probes.name":req.swagger.params.name.value},function(err, phone) {
+                    if(phone != null && err == null){
+                        var i = 0;
+                        while(i < phone.probes.length && phone.probes[i].name != req.swagger.params.name.value){
+                            i++;
+                        }
+                        res.json(phone.probes[i])
+                    }
+                    else{
+                        res.status(404).send();
+                    }
+                });
+            }
+            else{
                 res.status(404).send();
-              }
-            });
-          }
-          else{
-            res.status(404).send();
-          }
+            }
         });
-      });
-    }
+    });
+}
 // GET /users/{username}/devices/{uuid}/actions
 
 
 
 // POST /users/{username}/devices/{uuid}/actions
 
-    function postUsersDeviceUuidActions(req, res, next) {
-    	MongoClient.connect(url,  function(err, db) {
-    		assert.equal(null, err);
-    		console.log("Connected correctly to server");
-    		var actions = req.body;
+function postUsersDeviceUuidActions(req, res, next) {
+    MongoClient.connect(url,  function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        var actions = req.body;
         db.collection("phone").findOne({"uuid": req.swagger.params.uuid.value}, function(err, data){
-          console.log(data);
-          if ( data != null && data.actions[req.body.name]==null){
-              var tmp = "actions." + String([req.body.name]);
-              var updateAction = { "$set" : {[tmp] : req.body}};
-              db.collection("phone").update({"uuid" : req.swagger.params.uuid.value, }, updateAction, { upsert: true}, function(err2) {
-                if (!err2 ) res.json({success: 1, description: "Action Updated"})
-                else{
-                  res.status(409).send("");
-                }
-              });
-          }
-          else {
-              res.status(409).send("");
-          }
+            console.log(data);
+            if ( data != null && data.actions[req.body.name]==null){
+                var tmp = "actions." + String([req.body.name]);
+                var updateAction = { "$set" : {[tmp] : req.body}};
+                db.collection("phone").update({"uuid" : req.swagger.params.uuid.value, }, updateAction, { upsert: true}, function(err2) {
+                    if (!err2 ) res.json({success: 1, description: "Action Updated"})
+                    else{
+                        res.status(409).send("");
+                    }
+                });
+            }
+            else {
+                res.status(409).send("");
+            }
         });
-    	});
-    }
+    });
+}
--- Server/app.js
+++ Server/app.js
@@ -1,143 +1,136 @@
 'use strict';
 
-var config = {
-	appRoot: __dirname // required config
-};
-
-try {
-	process.chdir(config.appRoot); //on se place au bon endroit pour executer le processus
-}	
-catch (err) {
-	console.log('chdir: ' + err);
-}
-
 var SwaggerExpress = require('swagger-express-mw');
-var session = require('cookie-session');
+var session = require('express-session');
 var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
 var methodOverride = require('method-override');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var express = require('express')
-  , http = require('http')
-  , app = express()
-  , server = http.createServer(app);
+    , http = require('http')
+    , app = express()
+    , server = http.createServer(app);
 
 module.exports = app; // for testing
 
+var config = {
+    appRoot: __dirname // required config
+};
 
+/* On utilise les sessions */
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
 
+app.use('/node_modules', express.static(__dirname + '../node_modules'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname));
+app.use(methodOverride('_method'));
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+    if (err) { throw err; }
 
-  // install middleware
-  swaggerExpress.register(app);
+    // install middleware
+    swaggerExpress.register(app);
 
+    console.log("serveur bien lancé");
+    /* On redirige vers la todolist si la page demandée n'est pas trouvée */
 
-  app.use('/public', express.static(__dirname + '/public'));
-  app.use('/node_modules', express.static(__dirname + '/node_modules'));
-  app.set('view engine', 'ejs');
-  /* On utilise les sessions */
-  app.use(session({secret: 'todotopsecret'}));
-  app.use(express.static(__dirname));
-  app.use(methodOverride('_method'));
+    var username = "Pad";
+    var lastname = "Germain";
+    var firstname = "Alexandre";
+    var password = "mdp";
+    var sonne = true;
 
+    var user_session = new Object();
+    user_session.username = username;
+    user_session.lastname = lastname;
+    user_session.firstname = firstname;
+    user_session.email = "salut ca va ?";
 
-  console.log("serveur bien lancé");
-  /* On redirige vers la todolist si la page demandée n'est pas trouvée */
-
-  var username = "Pad";
-  var lastname = "Germain";
-  var firstname = "Alexandre";
-  var password = "mdp";
-  var sonne = true;
-
-  var user_session = new Object();
-  user_session.username = username;
-  user_session.lastname = lastname;
-  user_session.firstname = firstname;
-  user_session.email = "salut ca va ?";
-
-  /** Routes */
-  app.get('/', function(req, res){
-      if (typeof(req.session.info_user) != 'undefined') {
-          req.session.info_user = user_session;
-          res.render('index', {
-              username: user_session.username,
-              lastname: user_session.lastname,
-              firstname: user_session.firstname,
-              session: false});
-      }
-      else{
-          res.render('index', {
-              session: true,
-              username: "test"});
-      }
-  });
-
-  app.get('/api/users/:username', function (req, res){
-      res.render('gestion', {
-          username: user_session.username,
-          lastname: user_session.lastname,
-          firstname: user_session.firstname,
-          email: user_session.email});
-      console.log(user_session.username);
-  });
-
-  app.get('/api/users/login', function (req, res){
-      if(res.reponce == true){
-          res.render('gestion', {
-              username: user_session.username,
-              lastname: user_session.lastname,
-              firstname: user_session.firstname});
-      }
-      else {
-          res.render('index', {
-              username: user_session.username,
-              lastname: user_session.lastname,
-              firstname: user_session.firstname});
-      }
+    /** Routes */
+    app.get('/', function(req, res){
+        if (typeof(req.session.username) != 'undefined') {
+            res.render('index', {
+                username: user_session.username,
+                lastname: user_session.lastname,
+                firstname: user_session.firstname,
+                session: true});
+        }
+        else{
+            res.render('index', {
+                session: false,
+                username: "test"});
+        }
     });
 
-  app.get('*', function(req, res) {
-      res.send("erreur 404");
-  });
-
-
-  app.put('/api/users/:username',urlencodedParser, function(req, res) {
-     if (req.body.change_prenom != undefined && req.body.change_nom != undefined && req.body.change_prenom != ''
-          && req.body.change_nom != ''){
-          lastname = req.body.change_prenom;
-          firstname = req.body.change_nom;
-      }
-      if (req.body.password != undefined && req.body.password2 != undefined && req.body.password2 == req.body.password){
-          password = req.body.password;
-      }
-      console.log(req.body);
-      res.redirect('/api/users');
-  });
     /*
-  app.post('/api/users',urlencodedParser, function(req, res){
-      console.log(req.body);
-      if (req.body.username != '' && req.body.firstname != '' && req.body.lastname != ''
-          && req.body.password != '' && req.body.password2 != '' && req.body.email != ''){
-          user_session.username = req.body.username;
-          user_session.lastname = req.body.lastname;
-          user_session.firstname = req.body.firstname;
-          user_session.email = req.body.email;
-          user_session.password  = req.body.password;
-          res.redirect('api/users/' + req.body.username);
-      }
-      else{
-          res.redirect('/');
-      }
-  });
-  */
-  
-  //server.listen(8001,'192.168.21.55');
-  server.listen(3000,'127.0.0.1');
+    app.get('/api/users/:username', function (req, res){
+        res.render('gestion', {
+            username: user_session.username,
+            lastname: user_session.lastname,
+            firstname: user_session.firstname,
+            email: user_session.email});
+        console.log(user_session.username);
+    });
 
-  // if (swaggerExpress.runner.swagger.paths['/api']) {
-  //   console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  // }
+    app.get('/api/users/login', function (req, res){
+        if(res.reponce == true){
+            res.render('gestion', {
+                username: user_session.username,
+                lastname: user_session.lastname,
+                firstname: user_session.firstname});
+        }
+        else {
+            res.render('index', {
+                username: user_session.username,
+                lastname: user_session.lastname,
+                firstname: user_session.firstname});
+        }
+    });
+
+    app.get('*', function(req, res) {
+        res.send("erreur 404");
+    });
+
+
+    app.put('/api/users/:username',urlencodedParser, function(req, res) {
+        if (req.body.change_prenom != undefined && req.body.change_nom != undefined && req.body.change_prenom != ''
+            && req.body.change_nom != ''){
+            lastname = req.body.change_prenom;
+            firstname = req.body.change_nom;
+        }
+        if (req.body.password != undefined && req.body.password2 != undefined && req.body.password2 == req.body.password){
+            password = req.body.password;
+        }
+        console.log(req.body);
+        res.redirect('/api/users');
+    });
+    /*
+     app.post('/api/users',urlencodedParser, function(req, res){
+     console.log(req.body);
+     if (req.body.username != '' && req.body.firstname != '' && req.body.lastname != ''
+     && req.body.password != '' && req.body.password2 != '' && req.body.email != ''){
+     user_session.username = req.body.username;
+     user_session.lastname = req.body.lastname;
+     user_session.firstname = req.body.firstname;
+     user_session.email = req.body.email;
+     user_session.password  = req.body.password;
+     res.redirect('api/users/' + req.body.username);
+     }
+     else{
+     res.redirect('/');
+     }
+     });
+     */
+
+    //server.listen(8001,'192.168.21.55');
+    server.listen(3000,'127.0.0.1');
+
+    // if (swaggerExpress.runner.swagger.paths['/api']) {
+    //   console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+    // }
 });
