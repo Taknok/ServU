a584be37774f6be2c44a10446b871f7cdc30cf46
PitiEirb <gpitillion@bordeaux-inp.fr>
2017-05-17 18:22:49 +0200

    Get Event Skeletons v2 & Delete Event Skeletons OK !

--- Site/js/persoJS/controllerEventSkeleton.js
+++ Site/js/persoJS/controllerEventSkeleton.js
@@ -4,19 +4,44 @@
 
 rootApp
     .controller("EventSkeletonCtrl", function($scope, $http, $log, $compile, EventSkeletons, Alerts) {
-        function getListEventSkeleton() {
+        $scope.updateListEvent = function getListEventSkeleton() {
             EventSkeletons.listEventSkeletons().then(function listEventSkeletonOK(eventSkeletons) {
                 var alertGetSuccess = Alerts.alert('success', '<strong>GOOD</strong> Successful Recuperation of EventSkeletons');
                 $(".alertPlaceEventSkeleton").append(alertGetSuccess);
-                $scope.eventSkeletons = eventSkeletons.data.list;
+                $scope.eventSkeletons = eventSkeletons.data;
+                $log.log(eventSkeletons.data);
             }, function () {
-                /*localStorage.clear();
-                 document.location.href="#/log/connect";
-                 document.location.reload();*/
             });
-            window.setTimeout(getListEventSkeleton,15000);
+        };
+
+        $scope.deleteEventSkeleton = function(eventSkeleton) {
+            var eventSkeletonID = eventSkeleton.id;
+            EventSkeletons.deleteEvenetSkeleton(eventSkeletonID).then(function successCallback() {
+                var alertDeleteSuccess = Alerts.alert('success', '<strong>GOOD</strong> ' + eventSkeleton.label + ' deleted');
+                $(".alertPlaceEventSkeleton").append(alertDeleteSuccess);
+            }, function errorCallback(response) {
+                var errorValue = response.status;
+                if(errorValue === 404) { // Email already used
+                    var alertNotFounded = Alerts.alert('danger', '<strong>ERROR</strong> EventSkeleton or User not found');
+                    $(".alertPlaceEventSkeleton").append(alertNotFounded);
+                }
+                else if(errorValue === 400) { // Wrong Format
+                    var alertWrongFormat = Alerts.alert('danger', '<strong>ERROR</strong> Wrong Format');
+                    $(".alertPlaceEventSkeleton").append(alertWrongFormat);
+                }
+                else if (errorValue === 401) { // Unauthorized
+                    var alertUnauthorized = Alerts.alert('danger', '<strong>ERROR</strong> Unauthorized');
+                    $(".alertPlaceEventSkeleton").append(alertUnauthorized);
+                }
+                else if (errorValue === 403) { // Forbidden
+                    var alertForbidden = Alerts.alert('danger', '<strong>ERROR</strong> Forbidden');
+                    $(".alertPlaceEventSkeleton").append(alertForbidden);
+                }
+                else { // Unknown Issue
+                    var alertUnknown = Alerts.alert('danger', '<strong>ERROR</strong> Unknown Issue');
+                    $(".alertPlaceEventSkeleton").append(alertUnknown);
+                }
+            });
         }
 
-        $scope.username = 'Toto';
-        getListEventSkeleton();
     });
--- Site/js/persoJS/services.js
+++ Site/js/persoJS/services.js
@@ -17,19 +17,24 @@ rootApp
 
     .factory('EventSkeletons', function($http) {
 
-        var dataListEventSkeleton = {
-            username: username
-        };
-
         var reqListEventSkeleton = {
             method: 'GET',
             url: 'http://127.0.0.1:3000/api/users/' + username + '/eventSkeletons',
             json: true
         };
 
+
         return {
             listEventSkeletons: function () {
                 return $http(reqListEventSkeleton);
+            },
+            deleteEvenetSkeleton: function(id) {
+                var reqDeleteEventSkeleton = {
+                    method: 'DELETE',
+                    url: 'http://127.0.0.1:3000/api/users/' + username + '/eventSkeletons/' + id,
+                    json: true
+                };
+                return $http(reqDeleteEventSkeleton);
             }
         };
     });
