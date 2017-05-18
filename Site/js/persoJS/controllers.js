3687b07810eda169e29458dcaafbe538e98e824e
Florian Payet <f.pay80@gmail.com>
2017-05-18 20:49:40 +0200

    avancement passage en angularJs pour devices

--- Site/js/persoJS/controllers.js
+++ Site/js/persoJS/controllers.js
@@ -1,56 +1,81 @@
 
-rootApp.controller('devicesCtrl', function($scope, $uibModal) {
-    $scope.devices = devices;
-    $scope.moreDevice = devices[0];
+rootApp.controller('devicesCtrl', function($rootScope, $scope, $uibModal, $http, $filter) {
+
+    $scope.devices = [];
+    $scope.moreDevice = [];
+    $scope.nbPhones = 0;
+    $scope.nbTablets = 0;
 
     $scope.refreshDevices = function(){
-        alert('refresh');
+        $http.get(url+'/api/users/'+username+'/devices').then(function(res){
+            $scope.devices = res.data;
+            angular.forEach($scope.devices,function(device){
+                device.probes = {};
+                $http.get(url+'/api/users/'+username+'/devices/'+device.uuid+'/probes').then(function(res){
+                    angular.forEach(res.data,function(probe){
+                        device.probes[probe.name] = probe;
+                    });
+                },function(err){
+                    alert('Error while trying to get device\'s probes: ',err);
+                    console.log('Error : ',err);
+                })
+            });
+        },function(err){
+            alert('Error while trying to get user\'s devices: '+err);
+            console.log('Error : ',err);
+        }).then(function(){
+            $scope.countDevices();
+            console.log('Loaded devices: ',$scope.devices);
+            console.log('Refresh complete');
+        });
     };
 
     // Get a device given its uuid
     $scope.getDeviceByUuid = function(uuid){
-        for(var i = $scope.devices.length-1; i >= 0; i--){
-            if($scope.devices[i].uuid == uuid){
-                return $scope.devices[i];
-            }
-        };
+        return $filter('filter')($scope.devices,{'uuid':uuid});
     };
 
     // Computes the number of devices
-    $scope.nbPhones = 0;
-    $scope.nbTablets = 0;
-    angular.forEach(devices, function(device){
-        if(device.platform == 'mobile-phone'){$scope.nbPhones++;}
-        else if(device.platform == 'tablet'){$scope.nbTablets++;}
-    });
+    $scope.countDevices = function () {
+        $scope.nbPhones = 0;
+        $scope.nbTablets = 0;
+        for(var i = $scope.devices.length - 1; i >= 0; i--){
+            if($scope.devices[i].platform === 'mobile-phone'){$scope.nbPhones++}
+            else if($scope.devices[i].platform === 'tablet'){$scope.nbTablets++}
+        }
+    };
 
     // Delete a device
     $scope.deleteByUuid = function(uuid) {
-        for(var i = devices.length-1; i >= 0; i--){
-            if(devices[i].uuid == uuid){
-                devices.splice(i,1);
+        for(var i = $scope.devices.length-1; i >= 0; i--){
+            if($scope.devices[i].uuid == uuid){
+                $scope.devices.splice(i,1);
                 break;
             }
         };
         alert('deleted'+uuid);
     };
 
-    // Open the modal to obtain more info
-    $scope.openMoreModal = function(uuid) {
+    // More about device
+    $scope.changeMoreDevice = function(uuid) {
+        $scope.moreDevice = $scope.getDeviceByUuid(uuid)[0];
+        console.log('moreDevice changed');
+    };
 
-        var device = $scope.getDeviceByUuid(uuid);
-
-
+    $scope.getReadableTime = function(lastUpdate) {
+        return new Date(lastUpdate);
     };
 
     // Add the correct class to the battery bar
-    $scope.getBarTypeClassBoolean = function(battery,classStr) {
+    $scope.getBarTypeClassBoolean = function(percent,classStr) {
         switch(classStr) {
-            case 'primary': if(battery >= 95) {return true;} else {return false;} break;
-            case 'success': if(battery < 95 && battery >= 40) {return true;} else {return false;} break;
-            case 'warning': if(battery < 40 && battery >= 20) {return true;} else {return false;} break;
-            case 'danger' : if(battery < 20) {return true;} else {return false;} break;
+            case 'primary': if(percent >= 95) {return true;} else {return false;} break;
+            case 'success': if(percent < 95 && percent >= 40) {return true;} else {return false;} break;
+            case 'warning': if(percent < 40 && percent >= 20) {return true;} else {return false;} break;
+            case 'danger' : if(percent < 20) {return true;} else {return false;} break;
         }
     };
+
+    $scope.refreshDevices();
 
 });
